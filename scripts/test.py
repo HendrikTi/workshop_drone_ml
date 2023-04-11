b2810894b2d718ada2a8dccbf4a2c5331621f6d5
HendrikTi <h.timmermann98@gmail.com>
2023-04-11 13:25:08 +0200

    hiding files and added import test

---
+++ scripts/test.py
@@ -0,0 +1,13 @@
+try:
+    import numpy
+    import joblib
+    import pandas
+    import sklearn
+    import matplotlib
+    import serial
+    print("SUCCESS:All modules imported\n")
+except ImportError as e:
+    print("FAILURE:Error importing modules\n")
+
+
+
